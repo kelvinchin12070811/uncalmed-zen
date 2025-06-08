f3d2b329a2953367d1c4efe64efe701bd4822add
Kelvin Chin <chinkaiwen_35@outlook.com>
2025-06-08 18:39:16 +0800

    Remove temporary tab unload workaround

--- user.js
+++ user.js
@@ -8,8 +8,3 @@ user_pref("browser.ml.chat.provider", "https://gemini.google.com");
 
 // Re-enabled "Enhanced Tracking Protection" icon
 user_pref("zen.urlbar.show-protections-icon", true);
-
-// Temporary Workaround -- Tab unload
-user_pref("browser.low_commit_space_threshold_percent", 100);
-user_pref("browser.tabs.min_inactive_duration_before_unload", 600000); // 10 minutes in ms
-user_pref("browser.tabs.unloadOnLowMemory", true);
